67df76363c082f93157a8ce6ea28b834e0dc2c81
Stephen <medicup@gmail.com>
2018-01-15 09:35:50 -0500

    Update reset_master.py

--- reset_master.py
+++ reset_master.py
@@ -2,12 +2,7 @@
 # Author Stephen Kennedy
 import os
 
-user_path = os.getenv("HOME")
-reset = "reset_master.py"
-final_path = ("%s/%s" % (user_path, reset))
-
 os.system("git fetch origin")
 os.system("git reset --hard origin/master")
 os.system("git pull")
 os.system("chmod 755 master")
-#os.system("%s/master" % (final_path))
