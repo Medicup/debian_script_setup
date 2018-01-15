da1598f5a903b7cd1af2ee29b3725a99a24a7c77
Stephen <medicup@gmail.com>
2018-01-15 08:52:40 -0500

    Update reset_master.py
    updated path to run

--- reset_master.py
+++ reset_master.py
@@ -2,8 +2,11 @@
 # Author Stephen Kennedy
 import os
 
+user_path = os.getenv("HOME")
+final_path = ("%s/%s" % (user_path, myPython))
+
 os.system("git fetch origin")
 os.system("git reset --hard origin/master")
 os.system("git pull")
 os.system("chmod 755 master")
-os.system("./master")
+os.system("%s/master" (final_path))
